c6c43657f6c1f78d50ae7941effacf40a1983c49
Aravind M <blogsooner@gmail.com>
2015-01-25 21:18:12 -0800

    Adding more stopwords. Top level TopNWords.

--- HiSum/Story.cs
+++ HiSum/Story.cs
@@ -18,12 +18,15 @@ namespace HiSum
 
         public List<string> GetTopNWords(int N)
         {
-            string[] stopWords = new System.String[] { "me", "get", "has", "i", "over", "could", "have", "what", "a", "an", "and", "are", "as", "at", "be", "but", "by", "for", "if", "in", "into", "is", "it", "no", "not", "of", "on", "or", "such", "that", "the", "their", "then", "there", "these", "they", "this", "to", "was", "will", "with" };
+            string[] stopWords = new System.String[] {"he","his", "which","want","do", "would","more","like", "you","your","very", "me", "get", "has", "i", "over", "could", "have", "what", "a", "an", "and", "are", "as", "at", "be", "but", "by", "for", "if", "in", "into", "is", "it", "no", "not", "of", "on", "or", "such", "that", "the", "their", "then", "there", "these", "they", "this", "to", "was", "will", "with" };
             string[] allWords;
             Dictionary<string, int> wordCount = new Dictionary<string, int>();
+            List<string> topNWords = new List<string>();
             foreach (Comment comment in Comments)
             {
+                List<string> topNWordsForComment = new List<string>();
                 string tagLess = Util.StripTagsCharArray(comment.SubtreeText);
+                wordCount = new Dictionary<string, int>();
                 string[] separators = new string[] { " ", "." };
                 allWords = tagLess.Split(separators, StringSplitOptions.RemoveEmptyEntries);
                 foreach (string word in allWords)
@@ -38,8 +41,11 @@ namespace HiSum
                         wordCount[word] += 1;
                     }
                 }
+                topNWordsForComment = wordCount.OrderByDescending(x => x.Value).Select(x => x.Key).Take(N).ToList();
+                topNWordsForComment.Add(string.Empty);
+                topNWords.AddRange(topNWordsForComment);
             }
-            List<string> topNWords = wordCount.OrderByDescending(x => x.Value).Select(x => x.Key).Take(N).ToList();
+            //List<string> topNWords = wordCount.OrderByDescending(x => x.Value).Select(x => x.Key).Take(N).ToList();
             return topNWords;
         }
     }
