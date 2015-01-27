85b6fff3d927770dac4ef43831a35a6432743da8
Aravind M <blogsooner@gmail.com>
2015-01-26 23:38:34 -0800

    Trying Eto.Forms
    Not working yet

---
+++ EtoTest/MyForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtoTest
+{
+    class MyForm:Eto.Forms.Form
+    {
+        public MyForm()
+        {
+            // sets the client (inner) size of the window for your content
+            this.ClientSize = new Eto.Drawing.Size(600, 400);
+
+            this.Title = "Hello, Eto.Forms";
+        }
+    }
+}
