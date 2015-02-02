9008c021f7b0a5c11ca40e18f64c07ea659dd4a8
Aravind M <blogsooner@gmail.com>
2015-02-01 21:58:53 -0800

    Combining Eto forms and Reader output

--- HiSum.Forms/HiSumDisplay.cs
+++ HiSum.Forms/HiSumDisplay.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using Eto.Forms;
@@ -37,22 +38,44 @@ namespace HiSum.Forms
             data.Add(child2);
             child2.Children.Add(child3);
             view.DataStore = data;
-
+            var textbox = new TextBox() {Width = 1000};
+            var button = new Button(){Text = "Go", Width = 50};
+            var tbResult = new TextArea() {Width = 1000};
+            
+            button.Click += (sender, e) =>
+            {
+                string url = textbox.Text;
+                int storyID = Convert.ToInt32(url.Split('=')[1]);
+                Reader reader = new Reader();
+                Story story = reader.GetStory(storyID);
+                List<string> top10words = story.GetTopNWords(10);
+                foreach (string s in top10words)
+                {
+                    tbResult.Text += s + Environment.NewLine;
+                }
+            };
             Content = new TableLayout
             {
                 Spacing = new Size(5, 5), // space between each cell
                 Padding = new Padding(10, 10, 10, 10), // space around the table's sides
+                
                 Rows =
 				{
 					new TableRow(
                         new Label{Text = "Input URL from Hacker News: ",Width=200},
-						new TextBox(){Width = 1000},
-                        new Button(){Text = "Go", Width = 50}
+						textbox,
+                        button
 					),
+                    new TableRow(
+                        null,
+                        tbResult,
+                        null
+                        ),
 					new TableRow(
                         new Label(),
 						view
 					),
+                    
 					// by default, the last row & column will get scaled. This adds a row at the end to take the extra space of the form.
 					// otherwise, the above row will get scaled and stretch the TextBox/ComboBox/CheckBox to fill the remaining height.
 					new TableRow { ScaleHeight = true }
