998b089baf37c897df4d2d0755fc39c804108840
Aravind M <blogsooner@gmail.com>
2015-02-12 21:10:24 -0800

    PureCSS + Edge

---
+++ HiSum.App/reader.js
@@ -0,0 +1,9 @@
+﻿var edge = require('edge');
+var path = require('path');
+var dotNetFunction = edge.func({ assemblyFile: path.dirname(process.execPath) + '/dll/HiSum.dll' });
+function myFunction3() {
+    dotNetFunction(10, function (error, result) {
+        if (error) alert(error.message);
+        alert('Reply:' + result);
+    });
+}
